b395991321f1f4a1f459f771552f3fe712241418
H. Vetinari <h.vetinari@gmx.com>
2024-08-18 00:11:29 +1100

    skip tiff test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -13,6 +13,6 @@ dot -T? || true
 dot -Tpng -o sample.png sample.dot
 dot -Tpdf -o sample.pdf sample.dot
 dot -Tsvg -o sample.svg sample.dot
-dot -Ttiff -o sample.tiff sample.dot
+# dot -Ttiff -o sample.tiff sample.dot
 dot -Tjpeg -o sample.jpeg sample.dot
 sfdp -Tpdf -o sample.pdf sample.dot
