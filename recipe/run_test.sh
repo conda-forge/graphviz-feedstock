8489f6228ef2d3e05d828663a07e52f5a8eb1f57
H. Vetinari <h.vetinari@gmx.com>
2024-08-18 00:36:50 +1100

    one more pdf to disable

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -11,7 +11,7 @@ neato -?
 dot -T? || true
 
 dot -Tpng -o sample.png sample.dot
-dot -Tpdf -o sample.pdf sample.dot
+# dot -Tpdf -o sample.pdf sample.dot
 dot -Tsvg -o sample.svg sample.dot
 # dot -Ttiff -o sample.tiff sample.dot
 dot -Tjpeg -o sample.jpeg sample.dot
